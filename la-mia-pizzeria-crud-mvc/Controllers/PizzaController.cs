e87bacfc4c958d317e1d0ec49ae545b05eb2ba36
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-29 14:31:27 +0200

    creato PizzaController

---
+++ la-mia-pizzeria-crud-mvc/Controllers/PizzaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_crud_mvc.Models;  // Questa classe che richiamo, la sto artificialmente richiamando, non è stata scritta automaticamente
+using System.Diagnostics;  // Questa classe che richiamo, la sto artificialmente richiamando, non è stata scritta automaticamente
+
+namespace la_mia_pizzeria_crud_mvc.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserIndex()
+        {
+            return View();
+        }
+    }
+}
