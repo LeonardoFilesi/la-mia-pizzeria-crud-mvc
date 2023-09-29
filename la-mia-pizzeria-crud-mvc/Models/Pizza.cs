9aa5259b4b489eded7243d154275dcb9799e2489
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-29 17:41:23 +0200

    PizzaController non funziona

--- la-mia-pizzeria-crud-mvc/Models/Pizza.cs
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace la_mia_pizzeria_crud_mvc.Models
 {
     public class Pizza
     {
         [Key] public int Id { get; set; }
+
+        [MaxLength(100)]  // AGGIUNTA se si vogliono specificare ancora di più le colonne
         public string Name { get; set; }
+
+        [Column(TypeName = "text")]  // AGGIUNTA se si vogliono specificare ancora di più le colonne
         public string Description { get; set; }
         public int Price { get; set; }
         public string Image {  get; set; }
