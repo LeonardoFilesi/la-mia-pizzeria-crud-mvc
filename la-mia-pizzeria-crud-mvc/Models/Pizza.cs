fe064ce82533cf470e84ef98e5837b8f20eefff4
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-09-29 15:15:49 +0200

    migrations eseguite - database non updated (errore)

--- la-mia-pizzeria-crud-mvc/Models/Pizza.cs
+++ la-mia-pizzeria-crud-mvc/Models/Pizza.cs
@@ -2,16 +2,14 @@
 {
     public class Pizza
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
         public string Image {  get; set; }
 
 
-        public Pizza(int id, string name, string description, int price, string image)
+        public Pizza(string name, string description, int price, string image)
         {
-            this.Id = id;
             this.Name = name;
             this.Description = description;
             this.Price = price;
