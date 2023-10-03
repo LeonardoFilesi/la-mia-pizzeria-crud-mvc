d7ed10c976a302ba8950730dcda407e2d58f09d1
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-10-03 16:32:02 +0200

    logger perfezionati, index e userIndex corretti

--- la-mia-pizzeria-crud-mvc/CustomLoggers/CustomFileLogger.cs
+++ la-mia-pizzeria-crud-mvc/CustomLoggers/CustomFileLogger.cs
@@ -4,7 +4,7 @@
     {
         public void WriteLog(string message)
         {
-            File.WriteAllText("C:\\Utenti\\leona\\source\\repos\\la_mia_pizzeria_mvc\\la_mia_pizzeria_mvc\\my-file-logger.txt" , $"{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")}LOG: {message}\n");
+            File.WriteAllText("C:\\EsercizioLogger\\my-log.txt", $"{DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")}LOG: {message}\n");
         }
     }
 }
