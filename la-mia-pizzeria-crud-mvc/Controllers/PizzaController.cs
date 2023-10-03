5ab14281c5493224ee3876a3dc25dbc89bc84433
LeonardoFilesi <leonardo.filesi@gmail.com>
2023-10-03 10:25:16 +0200

    aggiunto Delete in View/Pizza & in PizzaController

--- la-mia-pizzeria-crud-mvc/Controllers/PizzaController.cs
+++ la-mia-pizzeria-crud-mvc/Controllers/PizzaController.cs
@@ -2,6 +2,7 @@
 using la_mia_pizzeria_crud_mvc.Models;  // Questa classe che richiamo, la sto artificialmente richiamando, non è stata scritta automaticamente
 using System.Diagnostics;  // Questa classe che richiamo, la sto artificialmente richiamando, non è stata scritta automaticamente
 using la_mia_pizzeria_crud_mvc.Database;
+using Microsoft.Identity.Client;
 
 namespace la_mia_pizzeria_crud_mvc.Controllers
 {
@@ -60,6 +61,73 @@ namespace la_mia_pizzeria_crud_mvc.Controllers
                 db.Pizzas.Add(newPizza);
                 db.SaveChanges();
                 return RedirectToAction("Index");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                Pizza pizzaToEdit = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault() as Pizza;
+
+                if(pizzaToEdit == null)
+                {
+                    return NotFound("La pizza non è stata trovata");
+                } else
+                {
+                    return View("Update", pizzaToEdit);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza updatedPizza)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View("Update", updatedPizza);
+            }
+
+            using(PizzaContext db = new PizzaContext())
+            {
+                Pizza pizzaToUpdate = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToUpdate != null)
+                {
+                    pizzaToUpdate.Name = updatedPizza.Name;
+                    pizzaToUpdate.Description = updatedPizza.Description;
+                    pizzaToUpdate.Image = updatedPizza.Image;
+                    pizzaToUpdate.Price = updatedPizza.Price;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                } else
+                {
+                    return NotFound("Mi spiace, non sono state trovate Pizze da aggiornare");
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                Pizza? pizzaToDelete = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if(pizzaToDelete != null)
+                {
+                    db.Pizzas.Remove(pizzaToDelete);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                } else
+                {
+                    return NotFound("La Pizza da eliminare non è stata trovata");
+                }
             }
         }
 
